81448b7be8b649d0e6809f688af46e83e37223a6
Subhash <barelasubhsh16@gmail.com>
2023-02-08 15:15:10 +0530

    push the  FlipCoin c# program

---
+++ FlipCoin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class FlipCoin
+    {
+       
+        public static void flip()
+        {
+            Random rnd = new Random();
+           
+            for(int i=0;i<4;i++)
+            {
+                Console.WriteLine(rnd.Next(3));//less than specifide
+            }
+           
+        }
+    }
+}
