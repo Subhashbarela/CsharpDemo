c2e51fba93c500417e0f3cad75970afb65a34fa2
Subhash <barelasubhsh16@gmail.com>
2023-02-08 14:34:31 +0530

    initial commit of main file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FlipCoin.flip();
+            
+            HermonicSe.Harmoni();
+
+            Console.WriteLine("hello c# program");
+        }
+    }
+}
